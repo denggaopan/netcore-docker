97b27b61d93c0972cbd6e3c49b99ca8b6375ee06
denggaopan <denggaopan@outlook.com>
2020-07-28 16:46:22 +0800

    1

---
+++ NetcoreDocker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace NetcoreDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.Read();
+        }
+    }
+}
